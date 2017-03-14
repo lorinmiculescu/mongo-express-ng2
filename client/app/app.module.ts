0d8111e6e68ad6a9de7fefd7dacb821e669e4571
Lorin Miculescu <lorinmiculescu@gmail.com>
2017-03-14 13:11:03 +0200

    added angular router

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,19 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: PathLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -12,7 +12,12 @@ let routes = function (app: express.Application) {
 
     app.use(express.static(path.join(__dirname, '../../client')));
     
-    app.route('*').get(errors[404]);
+    //app.route('*').get(errors[404]);
+    
+    // 404 error will be handled client-side
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/index.html'));
+    });
     
 
 };
